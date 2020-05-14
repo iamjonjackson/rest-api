c8ba9e97fdfd50939295d056cd2a0875cb4f40e9
Jon Jackson <jon@acuras.co.uk>
2020-05-14 17:18:04 +0100

    Implemented item update

--- class/Items.php
+++ class/Items.php
@@ -35,6 +35,24 @@ class Items
         }
         return false;		 
     }
-    
 
+    function update(){
+	 
+        $stmt = $this->conn->prepare("UPDATE items SET name= ?, description = ?, price = ?, category_id = ?, created = ? WHERE id = ?");
+     
+        $this->id = htmlspecialchars(strip_tags($this->id));
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->created = htmlspecialchars(strip_tags($this->created));
+     
+        $stmt->bind_param("ssiisi", $this->name, $this->description, $this->price, $this->category_id, $this->created, $this->id);
+        
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
+    }
+    
 }
