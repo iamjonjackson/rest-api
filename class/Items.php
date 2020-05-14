3593ff83469607634aa594ddcf65083340555af4
Jon Jackson <jon@acuras.co.uk>
2020-05-14 15:45:35 +0100

    Implement create items

--- class/Items.php
+++ class/Items.php
@@ -19,4 +19,25 @@ class Items
         return $result;	
     }
 
+    function create(){
+
+        $stmt = $this->conn->prepare("INSERT INTO items (`name`, `description`, `price`, `category_id`, `created`) VALUES (?,?,?,?,?)");
+        
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->description = htmlspecialchars(strip_tags($this->description));
+        $this->price = htmlspecialchars(strip_tags($this->price));
+        $this->category_id = htmlspecialchars(strip_tags($this->category_id));
+        $this->created = htmlspecialchars(strip_tags($this->created));
+        
+        $stmt->bind_param("ssiis", $this->name, $this->description, $this->price, $this->category_id, $this->created);
+        
+        if($stmt->execute()){
+
+            return true;
+        }
+
+        return false;		 
+    }
+    
+
 }
